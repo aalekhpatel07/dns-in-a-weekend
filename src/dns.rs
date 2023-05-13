725602551907b649e338dae815b4a0467bcca90c
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-05-13 17:37:35 -0500

    Implement parsing for dns packets.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/dns.rs
+++ src/dns.rs
@@ -1,13 +1,22 @@
+use std::io::Read;
+use std::io::Seek;
+use std::net::AddrParseError;
+use std::num::TryFromIntError;
+use std::string::FromUtf8Error;
+
 use bytes::Bytes;
-use bytes::BytesMut;
 use rand::prelude::*;
 use rand::thread_rng;
-use structure::{structure, structure_impl};
+use structure::byteorder::{
+    BigEndian,
+    ReadBytesExt,
+    WriteBytesExt
+};
 use thiserror::Error;
 
 pub type Int = u16;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct DNSHeader {
     pub id: Int,
     pub flags: DNSHeaderFlag,
@@ -18,39 +27,52 @@ pub struct DNSHeader {
 }
 
 pub trait ToBytes {
-    fn to_bytes(&self) -> Result<Bytes, Box<dyn std::error::Error>>;
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> Result<usize, DNSError>;
+}
+
+pub trait FromBytes: Sized {
+    type Error;
+    fn from_bytes<R: Read + Seek>(data: &mut R) -> Result<Self, Self::Error>;
 }
 
 impl ToBytes for DNSHeader {
-    fn to_bytes(&self) -> Result<Bytes, Box<dyn std::error::Error>> {
-        Ok(Bytes::from(
-            structure!("HHHHHH")
-                .pack(
-                    self.id,
-                    self.flags as Int,
-                    self.num_questions,
-                    self.num_answers,
-                    self.num_authorities,
-                    self.num_additionals,
-                )
-                .unwrap(),
-        ))
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> Result<usize, DNSError> {
+
+        writer.write_u16::<BigEndian>(self.id)?;
+        writer.write_u16::<BigEndian>(self.flags.into())?;
+        writer.write_u16::<BigEndian>(self.num_questions)?;
+        writer.write_u16::<BigEndian>(self.num_answers)?;
+        writer.write_u16::<BigEndian>(self.num_authorities)?;
+        writer.write_u16::<BigEndian>(self.num_additionals)?;
+
+        Ok(12)
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-#[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DNSRecordType {
     A = 1,
     // NS = 2,
     // MD = 3,
     // MF = 4,
-    // CNAME = 5,
+    CNAME = 5,
     // SOA = 6,
-    // add more
+    // add more,
 }
 
-#[derive(Debug, Clone, Copy)]
+impl TryFrom<Int> for DNSRecordType {
+    type Error = DNSError;
+    fn try_from(value: Int) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Self::A),
+            5 => Ok(Self::CNAME),
+            _ => Err(DNSError::BadRecordType(value))
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DNSRecordClass {
     IN = 1,
     CS = 2,
@@ -58,15 +80,45 @@ pub enum DNSRecordClass {
     HS = 4,
 }
 
-#[derive(Error, Debug)]
-pub enum DNSError<'a> {
-    #[error("Found a part with more than 255 chars in the url. (part: {part}, domain_name: {domain_name})")]
-    DomainNameHasTooLongPart { part: &'a str, domain_name: &'a str },
+impl TryFrom<Int> for DNSRecordClass {
+    type Error = DNSError;
+    fn try_from(value: Int) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Self::IN),
+            2 => Ok(Self::CS),
+            3 => Ok(Self::CH),
+            4 => Ok(Self::HS),
+            _ => Err(DNSError::BadRecordClass(value))
+        }
+    }
 }
 
-#[derive(Debug, Clone)]
+
+#[derive(Error, Debug)]
+pub enum DNSError {
+    #[error("Found a part with more than 255 chars in the url. (part: {part}, domain_name: {domain_name})")]
+    DomainNameHasTooLongPart { part: String, domain_name: String },
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error(transparent)]
+    InvalidUTF8(#[from] FromUtf8Error),
+    #[error("Could not recognize header flag: {0}")]
+    BadHeader(Int),
+    #[error("Could not recognize dns record type: {0}")]
+    BadRecordType(Int),
+    #[error("Could not recognize dns record class: {0}")]
+    BadRecordClass(Int),
+    #[error(transparent)]
+    IntTooLarge(#[from] TryFromIntError),
+    #[error(transparent)]
+    BadAddress(#[from] AddrParseError),
+    #[error("Couldn't find an ip address in the answer section.")]
+    NoIpAddressFound
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct DNSQuestion {
-    pub name: bytes::Bytes,
+    pub name: String,
     pub r#type: DNSRecordType,
     pub class: DNSRecordClass,
 }
@@ -76,27 +128,22 @@ impl DNSQuestion {
         domain_name: &str,
         r#type: DNSRecordType,
         class: DNSRecordClass,
-    ) -> Result<Self, DNSError<'_>> {
-        match encode::dns_name(domain_name) {
-            Ok(encoded) => Ok(Self {
-                name: encoded,
-                r#type,
-                class,
-            }),
-            Err(err) => Err(err),
+    ) -> Self {
+        Self {
+            name: domain_name.to_string(),
+            r#type,
+            class
         }
     }
 }
 
 impl ToBytes for DNSQuestion {
-    fn to_bytes(&self) -> Result<Bytes, Box<dyn std::error::Error>> {
-        let mut result = Vec::new();
-        result.extend(self.name.iter());
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> Result<usize, DNSError> {
+        let dns_name_size = encode::dns_name(writer, &self.name)?;
+        writer.write_u16::<BigEndian>(self.r#type as u16)?;
+        writer.write_u16::<BigEndian>(self.class as u16)?;
 
-        let s = structure!("HH");
-        let packed = s.pack(self.r#type as u16, self.class as u16).unwrap();
-        result.extend_from_slice(&packed);
-        Ok(Bytes::from(result))
+        Ok(dns_name_size + 4)
     }
 }
 
@@ -107,18 +154,42 @@ pub struct DNSQuery {
 }
 
 impl ToBytes for DNSQuery {
-    fn to_bytes(&self) -> Result<Bytes, Box<dyn std::error::Error>> {
-        let mut result = BytesMut::new();
-        result.extend_from_slice(&self.header.to_bytes()?);
-        result.extend_from_slice(&self.question.to_bytes()?);
-        Ok(result.into())
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> Result<usize, DNSError> {
+        let mut total_bytes = 0;
+        total_bytes += self.header.to_bytes(writer)?;
+        total_bytes += self.question.to_bytes(writer)?;
+
+        Ok(total_bytes)
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DNSHeaderFlag {
-    RecursionDesired = 1 << 8,
-    None = 0,
+    RecursionDesired,
+    None,
+    Other(Int)
+}
+
+
+impl From<DNSHeaderFlag> for Int {
+    fn from(value: DNSHeaderFlag) -> Self {
+        match value {
+            DNSHeaderFlag::None => 0,
+            DNSHeaderFlag::RecursionDesired => 1 << 8,
+            DNSHeaderFlag::Other(v) => v
+        }
+    }
+}
+
+
+impl From<Int> for DNSHeaderFlag {
+    fn from(value: Int) -> Self {
+        match value {
+            0 => Self::None,
+            256 => Self::RecursionDesired,
+            _ => Self::Other(value)
+        }
+    }
 }
 
 impl DNSQuery {
@@ -126,66 +197,278 @@ impl DNSQuery {
         domain_name: &str,
         record_type: DNSRecordType,
         record_class: DNSRecordClass,
-    ) -> Result<Self, DNSError> {
-        match DNSQuestion::new(domain_name, record_type, record_class) {
-            Ok(question) => {
-                let header_id = thread_rng().gen();
-                Ok(Self {
-                    question,
-                    header: DNSHeader {
-                        id: header_id,
-                        num_questions: 1,
-                        flags: DNSHeaderFlag::RecursionDesired,
-                        num_additionals: 0,
-                        num_answers: 0,
-                        num_authorities: 0,
-                    },
-                })
-            }
-            Err(err) => Err(err),
+    ) -> Self {
+        let question = DNSQuestion::new(domain_name, record_type, record_class);
+        let header_id = thread_rng().gen();
+        Self {
+            question,
+            header: DNSHeader {
+                id: header_id,
+                num_questions: 1,
+                flags: DNSHeaderFlag::RecursionDesired,
+                num_additionals: 0,
+                num_answers: 0,
+                num_authorities: 0,
+            },
         }
     }
 
     #[cfg(test)]
-    pub fn send_to_8_8_8_8(&self) -> Result<Bytes, Box<dyn std::error::Error>> {
+    pub fn send_to_8_8_8_8(&self) -> Result<Bytes, DNSError> {
         use std::net::{SocketAddr, UdpSocket};
 
-        let data = self.to_bytes()?.to_vec();
-        let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
-        let recipient = "8.8.8.8:53".parse::<SocketAddr>().unwrap();
+        let mut contents = vec![];
+        self.to_bytes(&mut contents)?;
+        let socket = UdpSocket::bind("0.0.0.0:0")?;
+        let recipient = "8.8.8.8:53".parse::<SocketAddr>()?;
 
-        socket
-            .send_to(&data, recipient)
-            .expect("to send to 8.8.8.8:53");
+        socket.send_to(&contents, recipient)?;
 
         let mut recv_buf = [0; 1024];
-        let (size, _) = socket.recv_from(&mut recv_buf).unwrap();
+        let (size, _) = socket.recv_from(&mut recv_buf)?;
 
         let observed = recv_buf[0..size].to_vec();
         Ok(Bytes::from(observed))
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DNSRecord {
+    pub name: String,
+    pub r#type: DNSRecordType,
+    pub class: DNSRecordClass,
+    pub ttl: u32,
+    pub data: Bytes
+}
+
+impl FromBytes for DNSHeader {
+    type Error = DNSError;
+    fn from_bytes<R: Read>(data: &mut R) -> Result<Self, Self::Error> {
+        Ok( Self {
+            id: data.read_u16::<BigEndian>()?,
+            flags: data.read_u16::<BigEndian>()?.into(),
+            num_questions: data.read_u16::<BigEndian>()?,
+            num_answers: data.read_u16::<BigEndian>()?,
+            num_additionals: data.read_u16::<BigEndian>()?,
+            num_authorities: data.read_u16::<BigEndian>()?
+        })
+    }
+}
+
+impl FromBytes for DNSQuestion {
+    type Error = DNSError;
+    fn from_bytes<R: Read + Seek>(reader: &mut R) -> Result<Self, Self::Error> {
+
+        match decode::dns_name_simple(reader) {
+            Ok((domain_name, _)) => {
+                let r#type = reader.read_u16::<BigEndian>()?;
+                let class = reader.read_u16::<BigEndian>()?;
+                Ok(Self {
+                    name: domain_name,
+                    r#type: r#type.try_into()?,
+                    class: class.try_into()?
+                })
+            },
+            Err(err) => {
+                Err(err)
+            }
+        }
+    }
+}
+
+
+impl FromBytes for DNSRecord {
+    type Error = DNSError;
+    fn from_bytes<R: Read + Seek>(reader: &mut R) -> Result<Self, Self::Error> {
+        
+        let (domain_name, _) = decode::dns_name(reader)?;
+
+        let r#type = reader.read_u16::<BigEndian>()?.try_into()?;
+        let class = reader.read_u16::<BigEndian>()?.try_into()?;
+        let ttl = reader.read_u32::<BigEndian>()?;
+        let data_len = reader.read_u16::<BigEndian>()?;
+
+        let mut buf = vec![0; data_len as usize];
+        reader.read_exact(&mut buf)?;
+        Ok(Self {
+            name: domain_name,
+            r#type,
+            class,
+            ttl,
+            data: Bytes::from(buf)
+        })
+    }
+}
+
+impl DNSRecord {
+
+    #[inline(always)]
+    pub fn ip(&self) -> Option<String> {
+        let octets = self.data.iter().map(|&x| format!("{x}")).collect::<Vec<String>>();
+        Some(octets.join("."))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct DNSPacket {
+    pub header: DNSHeader,
+    pub questions: Vec<DNSQuestion>,
+    pub answers: Vec<DNSRecord>,
+    pub authorities: Vec<DNSRecord>,
+    pub additionals: Vec<DNSRecord>
+}
+
+
+impl FromBytes for DNSPacket {
+    type Error = DNSError;
+    fn from_bytes<R: Read + Seek>(data: &mut R) -> Result<Self, Self::Error> {
+        let header = DNSHeader::from_bytes(data)?;
+        
+        let questions =
+        (0..header.num_questions)
+        .map(|_| {
+            DNSQuestion::from_bytes(data)
+        })
+        .collect::<Result<Vec<DNSQuestion>, DNSError>>()?;
+        
+        let answers =
+        (0..header.num_answers)
+        .map(|_| {
+            DNSRecord::from_bytes(data)
+        })
+        .collect::<Result<Vec<DNSRecord>, DNSError>>()?;
+
+        let authorities =
+        (0..header.num_authorities)
+        .map(|_| {
+            DNSRecord::from_bytes(data)
+        })
+        .collect::<Result<Vec<DNSRecord>, DNSError>>()?;
+
+        let additionals =
+        (0..header.num_additionals)
+        .map(|_| {
+            DNSRecord::from_bytes(data)
+        })
+        .collect::<Result<Vec<DNSRecord>, DNSError>>()?;
+
+        Ok(Self {
+            header,
+            answers,
+            additionals,
+            authorities,
+            questions
+        })
+    }
+}
+
+
+#[cfg(test)]
+pub fn lookup_domain(domain_name: &str) -> Result<String, DNSError> {
+    let query = DNSQuery::new(domain_name, DNSRecordType::A, DNSRecordClass::IN);
+    let response = query.send_to_8_8_8_8()?;
+    let mut reader = std::io::Cursor::new(response);
+    let packet = DNSPacket::from_bytes(&mut reader)?;
+    packet.answers[0].ip().ok_or(DNSError::NoIpAddressFound)
+}
+
+
 pub mod encode {
     use super::DNSError;
-    use bytes::Bytes;
+    use structure::byteorder::WriteBytesExt;
 
     /// Given a domain name encode it into bytes.
-    pub fn dns_name(domain_name: &str) -> Result<Bytes, DNSError> {
-        let mut encoded = Vec::new();
+    pub fn dns_name<W: std::io::Write>(writer: &mut W, domain_name: &str) -> Result<usize, DNSError> {
+        let mut total_bytes_written = 0;
+
         for part in domain_name.split('.') {
             let part_as_bytes = part.as_bytes();
-            let len = part_as_bytes.len();
-            if len > 255 {
-                return Err(DNSError::DomainNameHasTooLongPart { part, domain_name });
-            }
-            let len = len as u8;
+            let len: u8 = part_as_bytes.len().try_into()?;
 
-            encoded.push(len);
-            encoded.extend_from_slice(part_as_bytes);
+            writer.write_u8(len)?;
+            total_bytes_written += 1;
+
+            writer.write_all(part_as_bytes)?;
+            total_bytes_written += part_as_bytes.len();
+
         }
-        encoded.push(0);
+        writer.write_u8(0)?;
 
-        Ok(Bytes::from(encoded))
+        Ok(total_bytes_written)
+    }
+}
+
+pub mod decode {
+    use std::io::{Read, Seek, SeekFrom};
+    use super::DNSError;
+    use structure::byteorder::ReadBytesExt;
+
+    pub fn dns_name_simple<R: Read>(reader: &mut R) -> Result<(String, usize), DNSError> {
+        let mut parts = vec![];
+        let mut total_bytes_read: usize = 0;
+
+        loop {
+            let length = reader.read_u8()?;
+            total_bytes_read += 1;
+
+            if length == 0 {
+                break;
+            }
+            let mut buf = vec![0; length as usize];
+            reader.read_exact(&mut buf)?;
+            total_bytes_read += length as usize;
+
+            let part = String::from_utf8(buf)?;
+            parts.push(part);
+        }
+        Ok((parts.join("."), total_bytes_read))
+    }
+
+    pub fn dns_name<R: Read + Seek>(reader: &mut R) -> Result<(String, usize), DNSError> {
+
+        let mut parts = vec![];
+        let mut total_bytes_read: usize = 0;
+
+        loop {
+            let length = reader.read_u8()?;
+            total_bytes_read += 1;
+
+            if length == 0 {
+                break;
+            }
+
+            // Both of the two first bits set implies
+            // we need to decompress the value.
+            match length & 0b1100_0000 > 0 {
+                true => {
+                    let top_half = length & 0b0011_1111;
+                    let (part, bytes_read) = dns_name_compressed(reader, top_half)?;
+                    total_bytes_read += bytes_read;
+                    parts.push(part);
+                    break;
+                },
+                false => {
+                    let mut buf = vec![0; length as usize];
+                    reader.read_exact(&mut buf)?;
+                    total_bytes_read += length as usize;
+                    parts.push(String::from_utf8(buf)?);
+                }
+            }
+        }
+        Ok((parts.join("."), total_bytes_read))
+    }
+
+    pub fn dns_name_compressed<R: Read + Seek>(
+        reader: &mut R,
+        top_half: u8
+    ) -> Result<(String, usize), DNSError> {
+
+        let bottom_half = reader.read_u8()? as u16;
+        let pointer: u16 = ((top_half as u16) << 8) | bottom_half;
+        let current_position = reader.stream_position()?;
+        reader.seek(SeekFrom::Start(pointer as u64))?;
+        let (result, bytes_read) = dns_name(reader)?;
+        reader.seek(SeekFrom::Start(current_position))?;
+        Ok((result, bytes_read))
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,9 @@
 use crate::*;
-use bytes::Bytes;
+use std::io::{Cursor, Read};
+use test_case::{case, test_case};
 
 #[test]
-fn test_header_to_bytes() {
+fn test_header_serde() {
     let header = DNSHeader {
         id: 0x1314,
         flags: DNSHeaderFlag::None,
@@ -11,55 +12,194 @@ fn test_header_to_bytes() {
         num_authorities: 0,
         num_answers: 0,
     };
-    let observed = header.to_bytes().unwrap();
+
+    let mut observed = vec![];
+    header.to_bytes(&mut observed).unwrap();
     let expected = b"\x13\x14\0\0\0\x01\0\0\0\0\0\0";
-    assert_eq!(observed, Bytes::copy_from_slice(expected));
+    assert_eq!(observed, expected.to_vec());
+
+    let mut cursor = Cursor::new(observed);
+    let deserialized = DNSHeader::from_bytes(&mut cursor).unwrap();
+    assert_eq!(deserialized, header);
+
 }
 
 #[test]
-fn test_encode_dns_name() {
-    let observed = encode::dns_name("google.com").unwrap();
-    let expected = b"\x06google\x03com\0";
-    assert_eq!(observed, Bytes::from_static(expected));
+fn test_encode_and_decode_dns_name() {
+    let initial = "google.com";
+    let mut writer = vec![];
+    encode::dns_name(&mut writer, initial).unwrap();
+    let expected = b"\x06google\x03com\0".to_vec();
+    assert_eq!(writer, expected);
+
+    let mut reader = Cursor::new(expected);
+    let (decoded, _) = decode::dns_name(&mut reader).unwrap();
+    assert_eq!(decoded, initial);
 }
 
 #[test]
-fn test_dns_question_to_bytes() {
-    let question = DNSQuestion {
-        name: encode::dns_name("google.com").unwrap(),
-        r#type: DNSRecordType::A,
-        class: DNSRecordClass::IN,
-    };
+fn test_dns_question_serde() {
+    let question = DNSQuestion::new(
+        "google.com",
+        DNSRecordType::A,
+        DNSRecordClass::IN,
+    );
 
-    let observed = question.to_bytes().unwrap();
-    let expected = Bytes::from_static(b"\x06google\x03com\0\0\x01\0\x01");
-    assert_eq!(observed, expected);
-    // let expected = Bytes::from(b"a");
-}
+    let mut writer = vec![];
+    question.to_bytes(&mut writer).unwrap();
+    let expected = b"\x06google\x03com\0\0\x01\0\x01".to_vec();
+    assert_eq!(writer, expected);
 
-#[test]
-fn test_dns_question_new() {
-    let question = DNSQuestion::new("google.com", DNSRecordType::A, DNSRecordClass::IN).unwrap();
-    let observed = question.to_bytes().unwrap();
-    let expected = Bytes::from_static(b"\x06google\x03com\0\0\x01\0\x01");
-    assert_eq!(observed, expected);
+    let mut cursor = Cursor::new(expected);
+    let deserialized = DNSQuestion::from_bytes(&mut cursor).unwrap();
+    assert_eq!(deserialized, question);
+
 }
 
 #[test]
 fn test_dns_query_new() {
-    let query = DNSQuery::new("example.com", DNSRecordType::A, DNSRecordClass::IN).unwrap();
+    let query = DNSQuery::new(
+        "example.com", 
+        DNSRecordType::A, 
+        DNSRecordClass::IN
+    );
 
-    let observed = query.to_bytes().unwrap();
+    let mut observed = vec![];
+
+    query.to_bytes(&mut observed).unwrap();
     // except for the random id in the first two bytes,
     // everything should be fixed.
-    let expected_tail =
-        Bytes::from_static(b"\x01\0\0\x01\0\0\0\0\0\0\x07example\x03com\0\0\x01\0\x01");
+    let expected_tail = b"\x01\0\0\x01\0\0\0\0\0\0\x07example\x03com\0\0\x01\0\x01".to_vec();
     assert!(observed.ends_with(&expected_tail));
 }
 
 #[test]
 fn test_dns_query_roundtrip() {
-    let query = DNSQuery::new("www.example.com", DNSRecordType::A, DNSRecordClass::IN).unwrap();
+    let query = DNSQuery::new("www.example.com", DNSRecordType::A, DNSRecordClass::IN);
     let response = query.send_to_8_8_8_8().unwrap();
     assert!(response.ends_with(&[93, 184, 216, 34]));
+    let response = response.to_vec();
+
+    let mut cursor = Cursor::new(&response[0..12]);
+    let response_header = DNSHeader::from_bytes(&mut cursor).unwrap();
+    assert_eq!(response_header.flags, DNSHeaderFlag::Other(33152));
+    assert_eq!(response_header.num_questions, 1);
+    assert_eq!(response_header.num_answers, 1);
+}
+
+#[test]
+fn test_dns_query_record_parsing_roundtrip() {
+
+    let query = DNSQuery::new("www.example.com", DNSRecordType::A, DNSRecordClass::IN);
+    let response = query.send_to_8_8_8_8().unwrap();
+    assert!(response.ends_with(&[93, 184, 216, 34]));
+    let response = response.to_vec();
+
+    assert_eq!(response.len(), 49);
+}
+
+#[test]
+fn test_dns_name_simple() {
+    let query = DNSQuery::new("www.example.com", DNSRecordType::A, DNSRecordClass::IN);
+    let response = query.send_to_8_8_8_8().unwrap();
+
+    let mut cursor = Cursor::new(&response[..]);
+    let header = DNSHeader::from_bytes(&mut cursor).unwrap();
+    let question = DNSQuestion::from_bytes(&mut cursor).unwrap();
+
+    assert_eq!(question.class, DNSRecordClass::IN);
+    assert_eq!(question.r#type, DNSRecordType::A);
+    assert_eq!(question.name, "www.example.com".to_string());
+
+    assert_eq!(header.flags, DNSHeaderFlag::Other(33152));
+    assert_eq!(header.num_questions, 1);
+    assert_eq!(header.num_answers, 1);
+
+}
+
+
+#[test]
+fn test_dns_record_parsing() {
+    let response = vec![
+        96, 86, 129, 128, 
+        0, 1, 0, 1, 0, 0, 0, 0, 
+        3, 119, 119, 119, 7, 101, 120, 97, 
+        109, 112, 108, 101, 3, 99, 111, 109, 
+        0, 0, 1, 0, 1, 192, 12, 0, 
+        1, 0, 1, 0, 0, 82, 155, 0, 
+        4, 93, 184, 216, 
+        34
+    ];
+
+    let mut cursor = Cursor::new(&response[..]);
+    
+    let header = DNSHeader::from_bytes(&mut cursor).unwrap();
+    let question = DNSQuestion::from_bytes(&mut cursor).unwrap();
+
+    assert_eq!(cursor.position(), 33);
+    assert_eq!(question, DNSQuestion {
+        name: "www.example.com".into(),
+        r#type: DNSRecordType::A,
+        class: DNSRecordClass::IN
+    });
+    assert_eq!(header.flags, DNSHeaderFlag::Other(33152));
+    assert_eq!(header.num_questions, 1);
+    assert_eq!(header.num_answers, 1);
+    assert_eq!(header.num_authorities, 0);
+    assert_eq!(header.num_additionals, 0);
+
+    let record = DNSRecord::from_bytes(&mut cursor).unwrap();
+
+    assert_eq!(record, DNSRecord {
+        name: "www.example.com".into(),
+        r#type: DNSRecordType::A,
+        class: DNSRecordClass::IN,
+        ttl: 21147,
+        data: bytes::Bytes::from_static(b"]\xb8\xd8\"")
+    });
+}
+
+
+#[test]
+fn test_parse_dns_packet() {
+
+    let query = DNSQuery::new("www.example.com", DNSRecordType::A, DNSRecordClass::IN);
+    let response = query.send_to_8_8_8_8().unwrap();
+
+    let mut cursor = Cursor::new(&response[..]);
+    let packet = DNSPacket::from_bytes(&mut cursor).unwrap();
+
+    assert_eq!(packet.additionals.len(), 0);
+    assert_eq!(packet.authorities.len(), 0);
+    assert_eq!(packet.questions.len(), 1);
+    assert_eq!(packet.answers.len(), 1);
+
+
+    assert_eq!(packet.questions[0], DNSQuestion {
+        name: "www.example.com".into(),
+        r#type: DNSRecordType::A,
+        class: DNSRecordClass::IN
+    }); 
+
+    assert_eq!(packet.answers[0].class, DNSRecordClass::IN);
+    assert_eq!(packet.answers[0].r#type, DNSRecordType::A);
+    assert_eq!(packet.answers[0].name, "www.example.com".to_string());
+    assert_eq!(packet.answers[0].data, bytes::Bytes::from_static(b"]\xb8\xd8\""));
+
+    let header = packet.header;
+    assert_eq!(header.flags, DNSHeaderFlag::Other(33152));
+    assert_eq!(header.num_questions, 1);
+    assert_eq!(header.num_answers, 1);
+    assert_eq!(header.num_authorities, 0);
+    assert_eq!(header.num_additionals, 0);
+
+    assert_eq!(packet.answers[0].ip(), Some("93.184.216.34".to_string()));
+}
+
+
+#[test_case("www.example.com", "93.184.216.34"; "check example.com")]
+#[test_case("www.facebook.com", "9.115.116.97.114.45.109.105.110.105.4.99.49.48.114.192.16"; "check facebook.com")]
+fn test_lookup_domain(domain_name: &str, expected: &str) {
+    let observed = lookup_domain(domain_name).unwrap();
+    assert_eq!(observed, expected.to_string());
 }
